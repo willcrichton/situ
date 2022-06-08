ae97bef622a63a27a86b61e30095be1553e537af
Connor Fogarty <connorflynnfogarty@gmail.com>
2022-06-08 09:03:21 -0500

    Refactor event handlers and state in `editor.tsx` into class

--- frontend/packages/situ-classroom/lib/editor.tsx
+++ frontend/packages/situ-classroom/lib/editor.tsx
@@ -1,46 +1,17 @@
-import {
-  WebSocketMessageReader,
-  WebSocketMessageWriter,
-  toSocket,
-} from "@codingame/monaco-jsonrpc";
 import MonacoEditor from "@monaco-editor/react";
 import { action, reaction } from "mobx";
 import { observer, useLocalObservable } from "mobx-react";
 import { editor } from "monaco-editor";
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
-import {
-  CloseAction,
-  ErrorAction,
-  MessageTransports,
-  MonacoLanguageClient,
-  MonacoServices,
-} from "monaco-languageclient";
-import React, { useContext, useEffect, useRef } from "react";
+import { MonacoLanguageClient } from "monaco-languageclient";
+import React, { useContext } from "react";
 
 import { Action } from "./actions";
 import { ClientContext } from "./client";
 import { LessonContext } from "./lesson";
+import { EditorHandler } from "./lsp-client";
 
-// TODO: Move language client logic/config to separate file
-function createLanguageClient(transports: MessageTransports): MonacoLanguageClient {
-  return new MonacoLanguageClient({
-    name: "rust-analyzer",
-    clientOptions: {
-      documentSelector: [{ language: "rust", pattern: "**/*.rs" }],
-      errorHandler: {
-        error: () => ({ action: ErrorAction.Continue }),
-        closed: () => ({ action: CloseAction.DoNotRestart }),
-      },
-    },
-    connectionProvider: {
-      get: () => {
-        return Promise.resolve(transports);
-      },
-    },
-  });
-}
-
-enum EditorActionType {
+export enum EditorActionType {
   EditorChangeAction = 1,
   EditorSaveAction = 2,
 }
@@ -54,35 +25,33 @@ interface EditorSaveAction {
   subtype: EditorActionType.EditorSaveAction;
 }
 
-type EditorAction = Action & { type: "EditorAction" } & (EditorChangeAction | EditorSaveAction);
+export type EditorAction = Action & { type: "EditorAction" } & (
+    | EditorChangeAction
+    | EditorSaveAction
+  );
+
+export type EditorState = {
+  contents: string;
+  path: string | null;
+  langClient: MonacoLanguageClient | null;
+};
 
 export let Editor = observer(() => {
-  const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
-  const monacoRef = useRef<typeof monaco | null>(null);
-
   let lesson = useContext(LessonContext)!;
   let client = useContext(ClientContext)!;
 
-  function saveFile() {
-    let contents = editorRef.current!.getValue();
-    client.send({
-      type: "SaveFile",
-      path: state.path!,
-      contents,
-    });
-  }
-
-  let applyEdit = (contents: string) => {
-    const edits: monaco.editor.IIdentifiedSingleEditOperation[] = JSON.parse(contents);
-    editorRef.current?.executeEdits("recorder", edits);
-  };
+  let state = useLocalObservable<EditorState>(() => ({
+    contents: "",
+    path: null,
+    langClient: null,
+  }));
 
   function handleEditorDidMount(
     editor: editor.IStandaloneCodeEditor,
     monacoInstance: typeof monaco
   ) {
-    editorRef.current = editor;
-    monacoRef.current = monacoInstance;
+    let editorHandler = new EditorHandler(editor, monacoInstance, client, lesson, state);
+    editorHandler.registerEditorCallbacks();
 
     client.addListener(
       "FileContents",
@@ -92,111 +61,15 @@ export let Editor = observer(() => {
       })
     );
 
-    // Record changes made in editor
-    editorRef.current.onDidChangeModelContent(event => {
-      if (event.changes && lesson.isRecording()) {
-        let action: EditorAction = {
-          type: "EditorAction",
-          subtype: EditorActionType.EditorChangeAction,
-          contents: JSON.stringify(event.changes),
-        };
-        lesson.actions.addAction(action);
-      }
-    });
-
-    // Apply lesson modifications to editor
-    lesson.actions.addListener("EditorAction", (action: EditorAction) => {
-      if (action.subtype == EditorActionType.EditorChangeAction) {
-        applyEdit(action.contents);
-      } else if (action.subtype == EditorActionType.EditorSaveAction) {
-        saveFile();
-      }
-    });
-
-    // Save file on ctrl + s
-    editorRef.current.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS, function () {
-      saveFile();
-
-      if (lesson.isRecording()) {
-        let action: EditorAction = {
-          type: "EditorAction",
-          subtype: EditorActionType.EditorSaveAction,
-        };
-        lesson.actions.addAction(action);
-      }
-    });
-
-    monacoInstance?.languages.register({
-      id: "rust",
-      extensions: [".rs"],
-      aliases: ["rust", "rs", "RS", "Rust"],
-    });
-
-    MonacoServices.install(monacoInstance);
+    reaction(() => state.contents, editorHandler.handleContent);
   }
-
-  function handleContent(contents: string) {
-    if (!editorRef.current || !monacoRef.current) {
-      return;
-    }
-
-    const modelFile = monaco.Uri.parse(`file://${state.path!}`);
-    const existingModel = monacoRef.current.editor.getModel(modelFile);
-
-    // Update model content if exists, create one if not
-    if (existingModel) {
-      editorRef.current.setModel(existingModel);
-      existingModel.setValue(state.contents);
-    } else {
-      const model = monacoRef.current.editor.createModel(state.contents, "rust", modelFile);
-      editorRef.current.setModel(model);
-    }
-
-    if (!state.langClient) {
-      // TODO: if the user switches cargo projects, reinitialize LSP
-
-      // Connect to LSP port, passing current file path as query param
-      const url = `ws://localhost:8081?absPath=${state.path}`;
-      const webSocket = new WebSocket(url);
-
-      webSocket.onopen = () => {
-        const socket = toSocket(webSocket);
-        const reader = new WebSocketMessageReader(socket);
-        const writer = new WebSocketMessageWriter(socket);
-        const languageClient = createLanguageClient({
-          reader,
-          writer,
-        });
-        languageClient.start();
-        reader.onClose(() => languageClient.stop());
-
-        state.langClient = languageClient;
-      };
-
-      editorRef.current?.setValue(contents);
-    }
-  }
-
-  let state = useLocalObservable<{
-    contents: string;
-    path: string | null;
-    langClient: MonacoLanguageClient | null;
-  }>(() => ({
-    contents: "",
-    path: null,
-    langClient: null,
-  }));
-
-  useEffect(() => {
-    return reaction(() => state.contents, handleContent);
-  }, []);
 
   return (
     <div className="editor-wrapper">
       <div>
         File: <code>{state.path}</code>
       </div>
-      <MonacoEditor height={"40vh"} onMount={handleEditorDidMount} defaultValue={state.contents} />
+      <MonacoEditor height={"40vh"} onMount={handleEditorDidMount} />
     </div>
   );
 });
---
+++ frontend/packages/situ-classroom/lib/lsp-client.ts
@@ -0,0 +1,157 @@
+import {
+  WebSocketMessageReader,
+  WebSocketMessageWriter,
+  toSocket,
+} from "@codingame/monaco-jsonrpc";
+import { editor } from "monaco-editor";
+import * as monacoImport from "monaco-editor/esm/vs/editor/editor.api";
+import {
+  CloseAction,
+  ErrorAction,
+  MessageTransports,
+  MonacoLanguageClient,
+  MonacoServices,
+} from "monaco-languageclient";
+
+import { Client } from "./client";
+import { EditorAction, EditorActionType, EditorState } from "./editor";
+import { Lesson } from "./lesson";
+
+export class EditorHandler {
+  editor: editor.IStandaloneCodeEditor;
+  monaco: typeof monacoImport;
+  client: Client;
+  lesson: Lesson;
+  state: EditorState;
+
+  constructor(
+    editor: editor.IStandaloneCodeEditor,
+    monaco: typeof monacoImport,
+    client: Client,
+    lesson: Lesson,
+    state: EditorState
+  ) {
+    this.editor = editor;
+    this.monaco = monaco;
+    this.client = client;
+    this.lesson = lesson;
+    this.state = state;
+
+    monaco.languages.register({
+      id: "rust",
+      extensions: [".rs"],
+      aliases: ["rust", "rs", "RS", "Rust"],
+    });
+
+    MonacoServices.install(monaco);
+  }
+
+  applyEdit = (contents: string) => {
+    const edits = JSON.parse(contents);
+    this.editor?.executeEdits("recorder", edits);
+  };
+
+  saveFile = () => {
+    let contents = this.editor?.getValue();
+    this.client.send({
+      type: "SaveFile",
+      path: this.state.path!,
+      contents,
+    });
+  };
+
+  registerEditorCallbacks = () => {
+    // Record changes made in editor
+    this.editor?.onDidChangeModelContent(event => {
+      if (event.changes && this.lesson.isRecording()) {
+        let action: EditorAction = {
+          type: "EditorAction",
+          subtype: EditorActionType.EditorChangeAction,
+          contents: JSON.stringify(event.changes),
+        };
+        this.lesson.actions.addAction(action);
+      }
+    });
+
+    // Apply lesson modifications to editor
+    this.lesson.actions.addListener("EditorAction", (action: EditorAction) => {
+      if (action.subtype == EditorActionType.EditorChangeAction) {
+        this.applyEdit(action.contents);
+      } else if (action.subtype == EditorActionType.EditorSaveAction) {
+        this.saveFile();
+      }
+    });
+
+    // Save file on ctrl + s
+    this.editor?.addCommand(monacoImport.KeyMod.CtrlCmd | monacoImport.KeyCode.KeyS, () => {
+      this.saveFile();
+
+      if (this.lesson.isRecording()) {
+        let action: EditorAction = {
+          type: "EditorAction",
+          subtype: EditorActionType.EditorSaveAction,
+        };
+        this.lesson.actions.addAction(action);
+      }
+    });
+  };
+
+  handleContent = (contents: string) => {
+    const modelFile = this.monaco.Uri.parse(`file://${this.state.path!}`);
+    const existingModel = this.monaco.editor.getModel(modelFile);
+
+    // Update model content if exists, create one if not
+    if (existingModel) {
+      this.editor.setModel(existingModel);
+      existingModel.setValue(this.state.contents);
+    } else {
+      const model = this.monaco.editor.createModel(this.state.contents, "rust", modelFile);
+      this.editor.setModel(model);
+    }
+
+    if (!this.state.langClient) {
+      // TODO: if the user switches cargo projects, reinitialize LSP
+
+      // Connect to LSP port, passing current file path as query param
+      const url = `ws://localhost:8081?absPath=${this.state.path}`;
+      const webSocket = new WebSocket(url);
+
+      webSocket.onopen = () => {
+        const socket = toSocket(webSocket);
+        const reader = new WebSocketMessageReader(socket);
+        const writer = new WebSocketMessageWriter(socket);
+        const languageClient = createLanguageClient({
+          reader,
+          writer,
+        });
+
+        languageClient.start();
+        reader.onClose(() => languageClient.stop());
+
+        this.state.langClient = languageClient;
+      };
+
+      this.editor.setValue(contents);
+
+      webSocket.addEventListener("message", ev => console.log(ev.data));
+    }
+  };
+}
+
+export const createLanguageClient = (transports: MessageTransports): MonacoLanguageClient => {
+  return new MonacoLanguageClient({
+    name: "rust-analyzer",
+    clientOptions: {
+      documentSelector: [{ language: "rust", pattern: "**/*.rs" }],
+      errorHandler: {
+        error: () => ({ action: ErrorAction.Continue }),
+        closed: () => ({ action: CloseAction.DoNotRestart }),
+      },
+    },
+    connectionProvider: {
+      get: () => {
+        return Promise.resolve(transports);
+      },
+    },
+  });
+};
